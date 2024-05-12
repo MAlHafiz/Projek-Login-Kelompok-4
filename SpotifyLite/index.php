4ea4915aac78655e05cd8144f0e09a410658ef8b
MAlHafiz <145088498+MAlHafiz@users.noreply.github.com>
2024-05-12 20:12:08 +0700

    Add files via upload

---
+++ SpotifyLite/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Selamat Datang</title>
+</head>
+<body>
+    <h1>Ucapan Selamat Datang!</h1>
+    <p>Selamat datang di halaman ini. Ini adalah halaman sederhana yang menampilkan ucapan selamat datang.</p>
+</body>
+</html>
+
